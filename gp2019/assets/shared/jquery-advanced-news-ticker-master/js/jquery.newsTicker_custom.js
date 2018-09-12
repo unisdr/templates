f9dd9c90735aa61feee72f7e9aa5d484da46f7a6
Roberto Panzeri <robertopanzeri@rocketmail.com>
2018-09-12 19:05:38 +0200

    Home: settings for news ticker, up to two lines length

--- gp2019/assets/shared/jquery-advanced-news-ticker-master/js/jquery.newsTicker_custom.js
+++ gp2019/assets/shared/jquery-advanced-news-ticker-master/js/jquery.newsTicker_custom.js
@@ -44,12 +44,12 @@
 */
 
 ;
-(function($, window, document, undefined) {
+(function ($, window, document, undefined) {
         'use strict';
         var pluginName = 'newsTicker',
                 defaults = {
-                        row_height: 20,
-                        max_rows: 3,
+                        row_height: 46,
+                        max_rows: 1,
                         speed: 400,
                         duration: 2500,
                         direction: 'up',
@@ -59,18 +59,20 @@
                         prevButton: null,
                         startButton: null,
                         stopButton: null,
-												hasMoved: function(){
-													var visibleHeight = $('#updates-scroller li:visible').height();
-													visibleHeight = visibleHeight - 1;
-													$('#updates-scroller').animate({'height': visibleHeight}, 100);
-												},
-//                        hasMoved: function() {},
-                        movingUp: function() {},
-                        movingDown: function() {},
-                        start: function() {},
-                        stop: function() {},
-                        pause: function() {},
-                        unpause: function() {}
+                        hasMoved: function () {
+                                var visibleHeight = $('#updates-scroller li:visible').height();
+                                visibleHeight = visibleHeight - 1;
+                                $('#updates-scroller').animate({
+                                        'height': visibleHeight
+                                }, 100);
+                        },
+                        //                        hasMoved: function() {},
+                        movingUp: function () {},
+                        movingDown: function () {},
+                        start: function () {},
+                        stop: function () {},
+                        pause: function () {},
+                        unpause: function () {}
                 };
 
         function Plugin(element, options) {
@@ -89,46 +91,48 @@
         }
 
         Plugin.prototype = {
-                init: function() {
+                init: function () {
                         this.$el.height(this.options.row_height * this.options.max_rows)
-                                .css({overflow : 'hidden'});
+                                .css({
+                                        overflow: 'hidden'
+                                });
 
                         this.checkSpeed();
 
-                        if(this.options.nextButton && typeof(this.options.nextButton[0]) !== 'undefined')
-                                this.options.nextButton.click(function(e) {
+                        if (this.options.nextButton && typeof (this.options.nextButton[0]) !== 'undefined')
+                                this.options.nextButton.click(function (e) {
                                         this.moveNext();
                                         this.resetInterval();
                                 }.bind(this));
-                        if(this.options.prevButton && typeof(this.options.prevButton[0]) !== 'undefined')
-                                this.options.prevButton.click(function(e) {
+                        if (this.options.prevButton && typeof (this.options.prevButton[0]) !== 'undefined')
+                                this.options.prevButton.click(function (e) {
                                         this.movePrev();
                                         this.resetInterval();
                                 }.bind(this));
-                        if(this.options.stopButton && typeof(this.options.stopButton[0]) !== 'undefined')
-                                this.options.stopButton.click(function(e) {
+                        if (this.options.stopButton && typeof (this.options.stopButton[0]) !== 'undefined')
+                                this.options.stopButton.click(function (e) {
                                         this.stop()
                                 }.bind(this));
-                        if(this.options.startButton && typeof(this.options.startButton[0]) !== 'undefined')
-                                this.options.startButton.click(function(e) {
+                        if (this.options.startButton && typeof (this.options.startButton[0]) !== 'undefined')
+                                this.options.startButton.click(function (e) {
                                         this.start()
                                 }.bind(this));
-                        
-                        if(this.options.pauseOnHover) {
-                                this.$el.hover(function() {
+
+                        if (this.options.pauseOnHover) {
+                                this.$el.hover(function () {
                                         if (this.state)
                                                 this.pause();
-                                }.bind(this), function() {
+                                }.bind(this), function () {
                                         if (this.state)
                                                 this.unpause();
                                 }.bind(this));
                         }
 
-                        if(this.options.autostart)
+                        if (this.options.autostart)
                                 this.start();
                 },
 
-                start: function() {
+                start: function () {
                         if (!this.state) {
                                 this.state = 1;
                                 this.resetInterval();
@@ -136,7 +140,7 @@
                         }
                 },
 
-                stop: function() {
+                stop: function () {
                         if (this.state) {
                                 clearInterval(this.moveInterval);
                                 this.state = 0;
@@ -144,60 +148,66 @@
                         }
                 },
 
-                resetInterval: function() {
+                resetInterval: function () {
                         if (this.state) {
                                 clearInterval(this.moveInterval);
-                                this.moveInterval = setInterval(function() {this.move()}.bind(this), this.options.duration);
+                                this.moveInterval = setInterval(function () {
+                                        this.move()
+                                }.bind(this), this.options.duration);
                         }
                 },
 
-                move: function() {
-                         if (!this.paused) this.moveNext();
+                move: function () {
+                        if (!this.paused) this.moveNext();
                 },
 
-                moveNext: function() {
+                moveNext: function () {
                         if (this.options.direction === 'down')
                                 this.moveDown();
                         else if (this.options.direction === 'up')
                                 this.moveUp();
                 },
 
-                movePrev: function() {
+                movePrev: function () {
                         if (this.options.direction === 'down')
                                 this.moveUp();
                         else if (this.options.direction === 'up')
                                 this.moveDown();
                 },
 
-                pause: function() {
+                pause: function () {
                         if (!this.paused) this.paused = 1;
                         this.options.pause();
                 },
 
-                unpause: function() {
+                unpause: function () {
                         if (this.paused) this.paused = 0;
                         this.options.unpause();
                 },
 
-                moveDown: function() {
+                moveDown: function () {
                         if (!this.moving) {
                                 this.moving = 1;
                                 this.options.movingDown();
                                 this.$el.children('li:last').detach().prependTo(this.$el).css('marginTop', '-' + this.options.row_height + 'px')
-                                        .animate({marginTop: '0px'}, this.options.speed, function(){
+                                        .animate({
+                                                marginTop: '0px'
+                                        }, this.options.speed, function () {
                                                 this.moving = 0;
                                                 this.options.hasMoved();
                                         }.bind(this));
                         }
                 },
 
-                moveUp: function() {
+                moveUp: function () {
                         if (!this.moving) {
                                 this.moving = 1;
                                 this.options.movingUp();
                                 var element = this.$el.children('li:first');
-                                element.animate({marginTop: '-' + this.options.row_height + 'px'}, this.options.speed,
-                                        function(){
+                                element.animate({
+                                                marginTop: '-' + this.options.row_height + 'px'
+                                        }, this.options.speed,
+                                        function () {
                                                 element.detach().css('marginTop', '0').appendTo(this.$el);
                                                 this.moving = 0;
                                                 this.options.hasMoved();
@@ -205,39 +215,39 @@
                         }
                 },
 
-                updateOption: function(option, value) {
-                        if (typeof(this.options[option]) !== 'undefined'){
+                updateOption: function (option, value) {
+                        if (typeof (this.options[option]) !== 'undefined') {
                                 this.options[option] = value;
-                                if (option == 'duration' || option == 'speed'){
-                                    this.checkSpeed();
-                                    this.resetInterval();
+                                if (option == 'duration' || option == 'speed') {
+                                        this.checkSpeed();
+                                        this.resetInterval();
                                 }
                         }
                 },
 
-                add: function(content){
-                    this.$el.append($('<li>').html(content));
+                add: function (content) {
+                        this.$el.append($('<li>').html(content));
                 },
 
-                getState: function() {
+                getState: function () {
                         if (paused) return 2 // 2 = paused
-                        else return this.state;// 0 = stopped, 1 = started
+                        else return this.state; // 0 = stopped, 1 = started
                 },
 
-                checkSpeed: function() {
+                checkSpeed: function () {
                         if (this.options.duration < (this.options.speed + 25))
                                 this.options.speed = this.options.duration - 25;
                 },
 
-                destroy: function() {
+                destroy: function () {
                         this._destroy(); // or this.delete; depends on jQuery version
                 }
         };
 
-        $.fn[pluginName] = function(option) {
+        $.fn[pluginName] = function (option) {
                 var args = arguments;
-                
-                return this.each(function() {
+
+                return this.each(function () {
                         var $this = $(this),
                                 data = $.data(this, 'plugin_' + pluginName),
                                 options = typeof option === 'object' && option;
