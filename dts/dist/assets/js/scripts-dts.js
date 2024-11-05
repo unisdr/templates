15c711bb0d55e7025cb9c865bcb04dcf24609991
Roberto Panzeri <robertopanzeri@rocketmail.com>
2024-11-05 17:58:07 +0100

    Styles for Admin Account Setup flow (Data enter screen)

--- dts/dist/assets/js/scripts-dts.js
+++ dts/dist/assets/js/scripts-dts.js
@@ -10,22 +10,44 @@ const pwdFields = document.querySelectorAll("input[type='password']");
 if (pwdFields.length) {
     for (let i = 0; i < pwdFields.length; i++) {
         if (pwdFields[i].nextElementSibling.matches('.dts-form-component__pwd-toggle')) {
-            var pwdToggleBtn = pwdFields[i].nextElementSibling;
-            var pwdToggleImg = pwdToggleBtn.querySelector("img");
 
-            pwdToggleBtn.addEventListener("click", () => {
+            pwdFields[i].nextElementSibling.addEventListener("click", () => {
+                
                 if (pwdFields[i].type === "password") {
                     pwdFields[i].type = "text";
-                    pwdToggleImg.src = "assets/icons/eye-hide-password.svg";
+                    pwdFields[i].nextElementSibling.querySelector("img").src = "assets/icons/eye-hide-password.svg";
                 } else {
                     pwdFields[i].type = "password";
-                    pwdToggleImg.src = "assets/icons/eye-show-password.svg";
+                    pwdFields[i].nextElementSibling.querySelector("img").src = "assets/icons/eye-show-password.svg";
                 }
             });
         }
     }
 }
 
+// Submit button enabling only when required fields are filled
+const requiredFields = document.querySelectorAll("input[required]");
+const submitButton = document.querySelector("button[type='submit']");
+
+if (requiredFields.length) {
+    var requiredFilled;
+
+    for (let i = 0; i < requiredFields.length; i++) {
+        requiredFields[i].addEventListener("input", () => {
+            requiredFilled = true;
+            for (let j = 0; j < requiredFields.length; j++) {
+                if (!requiredFields[j].validity.valid) {
+                    requiredFilled = false;
+                }
+            }
+            if (requiredFilled) {
+                submitButton.disabled = false;
+            } else {
+                submitButton.disabled = true;
+            }
+        });
+    }
+}
 
 /***/ }),
 /* 2 */
